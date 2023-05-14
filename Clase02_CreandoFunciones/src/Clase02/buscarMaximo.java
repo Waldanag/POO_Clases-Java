fc05746852d8b5823ddeb5eb6c029c18be4c4061
Waldanag <walter@algabits.com>
2023-05-13 22:38:00 -0400

    mas ejercicios de funciones

--- Clase02_CreandoFunciones/src/Clase02/buscarMaximo.java
+++ Clase02_CreandoFunciones/src/Clase02/buscarMaximo.java
@@ -1,9 +1,44 @@
 package Clase02;
 
+import java.util.Scanner;
+
+/*INGRESAR 3 NUMEROS Y VER CUAL ES EL MAYOR*/
 public class buscarMaximo {
 
-    
+    public static Integer maximoEntreTresNumeros(Integer numA, Integer numB, Integer
+            numC){
 
+        if (numA > numB && numA > numC){
+            return numA;
+        } else if (numB > numA && numB > numC) {
+            return numB;
+        } else {
+            return numC;
+        }
+    }
+
+    public static void main(String[] args){
+
+        Integer numA;
+        Integer numB;
+        Integer numC;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese un 1er número");
+        numA = scanner.nextInt();
+        System.out.println("Ingrese un 2do número");
+        numB = scanner.nextInt();
+        System.out.println("Ingrese un 3er número");
+        numC = scanner.nextInt();
+
+        //Integer comparacion = maximoEntreTresNumeros(numA, numB, numC);
+        //System.out.println(comparacion);
+
+        System.out.println("El numero mayor es el " + maximoEntreTresNumeros(numA, numB, numC));
+
+    }
 
 
 }
+
+
---
+++ Clase02_CreandoFunciones/src/Clase02/textosDistintos.java
@@ -0,0 +1,38 @@
+package Clase02;
+
+import java.util.Scanner;
+
+public class textosDistintos {
+
+    public static boolean cadenasDeTextoDistintas(String textoA, String textoB){
+
+        if (textoA.equals(textoB)){
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+
+        String textoA;
+        String textoB;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingresa el 1er texto");
+        textoA = scanner.next();
+        System.out.println("Ingresa el 2do texto");
+        textoB = scanner.next();
+
+        Boolean comparacion = cadenasDeTextoDistintas(textoA, textoB);
+        System.out.println(comparacion);
+
+        if (comparacion = true){
+            System.out.println("Los textos no coinciden");
+        } else {
+            System.out.println("Los textos coinciden");
+        }
+
+    }
+
+
+}
